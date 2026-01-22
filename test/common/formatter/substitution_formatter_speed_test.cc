7befc03f151ad583685a6c7707d710920bc2bcdc
John Howard <john.howard@solo.io>
2026-01-22 10:36:17 -0800

    wip cel bench

--- test/common/formatter/substitution_formatter_speed_test.cc
+++ test/common/formatter/substitution_formatter_speed_test.cc
@@ -2,8 +2,10 @@
 #include "source/common/formatter/substitution_formatter.h"
 #include "source/common/network/address_impl.h"
 
+#include "source/extensions/formatter/cel/cel.h"
 #include "test/common/stream_info/test_util.h"
 #include "test/mocks/http/mocks.h"
+#include "test/mocks/server/factory_context.h"
 
 #include "benchmark/benchmark.h"
 
@@ -56,26 +58,84 @@ static void BM_AccessLogFormatterSetup(benchmark::State& state) {
 BENCHMARK(BM_AccessLogFormatterSetup);
 
 // NOLINTNEXTLINE(readability-identifier-naming)
-static void BM_AccessLogFormatter(benchmark::State& state) {
+static void BMJ_NATIVE(benchmark::State& state) {
   testing::NiceMock<MockTimeSystem> time_system;
 
+  Http::TestRequestHeaderMapImpl request_header{{"header", "value"}};
+  Formatter::Context formatter_context;
+  formatter_context.setRequestHeaders(request_header);
+  NiceMock<Server::Configuration::MockFactoryContext> context_;
+  ScopedThreadLocalServerContextSetter server_context_singleton_setter_{
+      context_.server_factory_context_};
+
   std::unique_ptr<Envoy::TestStreamInfo> stream_info = makeStreamInfo(time_system);
-  static const char* LogFormat =
-      "%DOWNSTREAM_REMOTE_ADDRESS_WITHOUT_PORT% %START_TIME(%Y/%m/%dT%H:%M:%S%z %s)% "
-      "%REQ(:METHOD)% "
-      "%REQ(X-FORWARDED-PROTO)%://%REQ(:AUTHORITY)%%REQ(X-ENVOY-ORIGINAL-PATH?:PATH)% %PROTOCOL% "
-      "s%RESPONSE_CODE% %BYTES_SENT% %DURATION% %REQ(REFERER)% \"%REQ(USER-AGENT)%\" - - -\n";
+  static const char* LogFormat = "%REQ(header)%";
 
   std::unique_ptr<Envoy::Formatter::FormatterImpl> formatter =
       *Envoy::Formatter::FormatterImpl::create(LogFormat, false);
 
+  const std::string output = formatter->format(formatter_context, *stream_info);
+  std::cout << output << std::endl; // Print the output to the console
+  size_t output_bytes = 0;
+  for (auto _ : state) { // NOLINT: Silences warning about dead store
+    output_bytes += formatter->format(formatter_context, *stream_info).length();
+  }
+  benchmark::DoNotOptimize(output_bytes);
+}
+BENCHMARK(BMJ_NATIVE);
+static void BMJ_CEL(benchmark::State& state) {
+  testing::NiceMock<MockTimeSystem> time_system;
+
+  Http::TestRequestHeaderMapImpl request_header{{"header", "value"}};
+  Formatter::Context formatter_context;
+  formatter_context.setRequestHeaders(request_header);
+  NiceMock<Server::Configuration::MockFactoryContext> context_;
+  ScopedThreadLocalServerContextSetter server_context_singleton_setter_{
+      context_.server_factory_context_};
+  std::unique_ptr<Envoy::TestStreamInfo> stream_info = makeStreamInfo(time_system);
+  static const char* LogFormat = "%CEL(request.headers['header'])%";
+
+  std::unique_ptr<Envoy::Formatter::FormatterImpl> formatter =
+      *Envoy::Formatter::FormatterImpl::create(LogFormat, false);
+
+  const std::string output = formatter->format({}, *stream_info);
+  std::cout << output << std::endl; // Print the output to the console
+  size_t output_bytes = 0;
+  absl::optional<size_t> max_length = absl::nullopt;
+  auto cel_parser = std::make_unique<Extensions::Formatter::CELFormatterCommandParser>();
+  auto ff = cel_parser->parse("CEL", "request.headers['header']", max_length);
+  EXPECT_THAT(ff->formatValue(formatter_context, *stream_info),
+              ProtoEq(ValueUtil::stringValue("value")));
+  for (auto _ : state) { // NOLINT: Silences warning about dead store
+    ff->formatValue(formatter_context, *stream_info);
+  }
+  benchmark::DoNotOptimize(output_bytes);
+}
+BENCHMARK(BMJ_CEL);
+static void BMJ_BASELINE(benchmark::State& state) {
+  testing::NiceMock<MockTimeSystem> time_system;
+
+  Http::TestRequestHeaderMapImpl request_header{{"header", "value"}};
+  Formatter::Context formatter_context;
+  formatter_context.setRequestHeaders(request_header);
+  NiceMock<Server::Configuration::MockFactoryContext> context_;
+  ScopedThreadLocalServerContextSetter server_context_singleton_setter_{
+      context_.server_factory_context_};
+  std::unique_ptr<Envoy::TestStreamInfo> stream_info = makeStreamInfo(time_system);
+  static const char* LogFormat = "";
+
+  std::unique_ptr<Envoy::Formatter::FormatterImpl> formatter =
+      *Envoy::Formatter::FormatterImpl::create(LogFormat, false);
+
+  const std::string output = formatter->format({}, *stream_info);
+  std::cout << output << std::endl; // Print the output to the console
   size_t output_bytes = 0;
   for (auto _ : state) { // NOLINT: Silences warning about dead store
     output_bytes += formatter->format({}, *stream_info).length();
   }
   benchmark::DoNotOptimize(output_bytes);
 }
-BENCHMARK(BM_AccessLogFormatter);
+BENCHMARK(BMJ_BASELINE);
 
 // NOLINTNEXTLINE(readability-identifier-naming)
 static void BM_AccessLogFormatterTextMockJson(benchmark::State& state) {
